fef5f45b4d2dee4e1cdb16cc22fb896da6a0bed9
kcm7577-del <kcm7577@gmail.com>
2026-06-26 20:48:15 +0900

    Update check.py

--- check.py
+++ check.py
@@ -1,11 +1,12 @@
-from playwright.sync_api import sync_playwright
-import requests
 import os
+import requests
+from playwright.sync_api import sync_playwright
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
 
 URL = "https://casper.hyundai.com/ev-guide/eco-incentive"
+
 
 def send(msg):
     requests.post(
@@ -13,17 +14,62 @@ def send(msg):
         data={
             "chat_id": CHAT_ID,
             "text": msg
-        }
+        },
+        timeout=30
     )
 
+
 with sync_playwright() as p:
-    browser = p.chromium.launch(headless=True)
-    page = browser.new_page()
+
+    browser = p.chromium.launch(
+        headless=True
+    )
+
+    context = browser.new_context()
+
+    page = context.new_page()
 
     page.goto(URL)
 
-    page.wait_for_timeout(2000)
+    page.get_by_role("button", name="닫기").click()
 
-    print("페이지 접속 완료")
+    page.get_by_role("textbox", name="시/도 선택").click()
 
-    browser.close()
+    page.get_by_role("listitem").filter(
+        has_text="경북"
+    ).click()
+
+    page.get_by_role("textbox", name="시/군 선택").click()
+
+    page.get_by_role("listitem").filter(
+        has_text="칠곡군"
+    ).click()
+
+    page.get_by_role(
+        "button",
+        name="조회하기"
+    ).click()
+
+    page.wait_for_timeout(3000)
+    status = ""
+
+    if page.get_by_text("가능").count() > 0:
+        status = "가능"
+
+    elif page.get_by_text("임시중단/마감").count() > 0:
+        status = "임시중단/마감"
+
+    else:
+        status = "알수없음"
+
+    print(status)
+
+    if status == "가능":
+        send(
+            "🚗 칠곡군 전기차 보조금 신청 가능합니다!\n\n"
+            "https://casper.hyundai.com/ev-guide/eco-incentive"
+        )
+
+    context.close()
+
+    browser.close()    
